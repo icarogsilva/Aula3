7d51842e577156e352a691dadfcc28a3c4481840
icarogsilva <icaro_g_silva@hotmail.com>
2020-08-10 20:34:27 -0300

    Projeto Criado

---
+++ src/model/services/RentalService.java
@@ -0,0 +1,42 @@
+package model.services;
+
+import model.entities.CarRental;
+import model.entities.Invoice;
+
+public class RentalService {
+	
+	private Double pricePerDay;
+	private Double pricePerHour;
+	
+	private BrazilTaxServices taxServices;
+	
+	
+	public RentalService(Double pricePerDay, Double pricePerHour, BrazilTaxServices taxServices) {
+		super();
+		this.pricePerDay = pricePerDay;
+		this.pricePerHour = pricePerHour;
+		this.taxServices = taxServices;
+	};
+	
+	public void processInvoice (CarRental carRental) {
+		
+		long t1 = carRental.getStart().getTime();
+		long t2 = carRental.getFinish().getTime();
+		double hours = (double)(t2 - t1) / 1000 / 60 / 60;
+		
+		double basicPayment;
+		if (hours <= 12) {
+			basicPayment = Math.ceil(hours) * pricePerHour;
+		}
+		else {
+			basicPayment = Math.ceil(hours) * pricePerDay;
+		}
+		
+		
+		double tax = taxServices.tax(basicPayment);
+		
+		carRental.setInvoice(new Invoice(basicPayment, tax));
+		
+	}
+
+}
